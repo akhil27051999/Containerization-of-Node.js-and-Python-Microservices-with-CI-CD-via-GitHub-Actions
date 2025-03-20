7e151dd2239391be9842cb6d4e21236c71d35306
Akhil Thyadi <thyadiakhil@gmail.com>
2025-03-20 20:42:33 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+// Importing React library to use JSX and React functionality
 import React from 'react';
 
+// Functional component definition for the main App
 function App() {
   return (
+    // The outer div container that wraps the content
     <div>
+      {/* Heading element that displays a message on the page */}
       <h1>Hello from React Frontend!</h1>
     </div>
   );
 }
 
+// Exporting the App component so it can be used in other parts of the application
 export default App;
